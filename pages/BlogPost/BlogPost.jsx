def399336822291d5d3c429994f304b3e439e811
anwarul <anwarulhoque032@gmail.com>
2023-02-18 00:18:08 +0600

    learn basic  routing in next js

---
+++ pages/BlogPost/BlogPost.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BlogPost = () => {
+    return (
+        <div>
+            <h1>Something under Something</h1>
+        </div>
+    );
+};
+
+export default BlogPost;
---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const about = () => {
+    return (
+        <div>
+           <h1>This our About Section!</h1> 
+        </div>
+    );
+};
+
+export default about;
